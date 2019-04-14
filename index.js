ea7343586e7df2c25f5e47041ff13926e6fe7d0f
shumukh <shumukh.alshuaibi@gmail.com>
2019-04-14 23:44:05 +0300

    jQuery hw

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-// turn on stop function 
-function turnOnStopLight() {
-    // change background color of stop light to red
-    // changes background color of green and yellow to black
+
+
+document.getElementById('stopButton').onclick = turnOnStopLightR;
+document.getElementById('slowButton').onclick = turnOnStopLightY;
+document.getElementById('goButton').onclick = turnOnStopLightG;
+
+function turnOnStopLightR() {
+  clearLights();
+  document.getElementById('stopLight').style.backgroundColor = "red";
 }
 
-// turn on slow function
-function turnOnSlowLight() { 
-  // change background color of slow light to yellow
-  // changes background color of red and green to black
+function turnOnStopLightY() {
+  clearLights();
+  document.getElementById('slowLight').style.backgroundColor = "yellow";
 }
 
-// turn on go function
-function turnOnGoLight() {
-    // change background color of go light to green
-    // changes background color of red and yellow to black
+function turnOnStopLightG() {
+  clearLights();
+  document.getElementById('goLight').style.backgroundColor = "green";
 }
 
-// set stop light click event
-
-// set slow light click event
-
-// set go light click event
-
+function clearLights() {
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+}
